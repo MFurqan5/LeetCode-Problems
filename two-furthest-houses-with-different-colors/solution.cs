9d31252558fe11e6fcf564b6451610a51c09edd1
MUHAMMAD FURQAN MUSTAFA <muhammadfurqanmustafa9@gmail.com>
2026-09-13 21:40:17 +0500

    Sync: Two Furthest Houses With Different Colors (Easy)

---
+++ two-furthest-houses-with-different-colors/solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxDistance(int[] colors) {
+        int n = colors.Length;
+        int maxDist = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[0]) {
+                maxDist = Math.Max(maxDist, i);
+            }
+            if (colors[i] != colors[n - 1]) {
+                maxDist = Math.Max(maxDist, n - 1 - i);
+            }
+        }
+        
+        return maxDist;
+    }
+}
