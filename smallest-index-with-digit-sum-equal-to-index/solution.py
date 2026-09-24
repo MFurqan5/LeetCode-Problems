59a50a59057a7273cac6a617cf8893a42631cd07
MUHAMMAD FURQAN MUSTAFA <muhammadfurqanmustafa9@gmail.com>
2026-09-24 20:54:05 +0500

    Sync: Smallest Index With Digit Sum Equal to Index (Easy)

---
+++ smallest-index-with-digit-sum-equal-to-index/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def smallestIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i, num in enumerate(nums):
+            s = 0
+            while num:
+                s += num % 10
+                num //= 10
+            if s == i:
+                return i
+        return -1
